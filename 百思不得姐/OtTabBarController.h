5da28561c43a022c9f9ac72d1407a06aa7055e27
Orient0128 <767741560@qq.com>
2016-08-19 08:50:28 +0800

    基本配置

---
+++ 百思不得姐/OtTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  OtTabBarController.h
+//  百思不得姐
+//
+//  Created by Orient on 16/8/19.
+//  Copyright © 2016年 Orient. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OtTabBarController : UITabBarController
+
+@end
---
+++ 百思不得姐/OtTabBarController.m
@@ -0,0 +1,39 @@
+//
+//  OtTabBarController.m
+//  百思不得姐
+//
+//  Created by Orient on 16/8/19.
+//  Copyright © 2016年 Orient. All rights reserved.
+//
+
+#import "OtTabBarController.h"
+
+@interface OtTabBarController ()
+
+@end
+
+@implementation OtTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
