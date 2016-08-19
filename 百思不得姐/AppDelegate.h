7d1e3b24ad5066eb4782e95516151c77ae7b8872
Orient0128 <767741560@qq.com>
2016-08-19 08:38:23 +0800

    Initial Commit

---
+++ 百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by Orient on 16/8/19.
+//  Copyright © 2016年 Orient. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 百思不得姐/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  百思不得姐
+//
+//  Created by Orient on 16/8/19.
+//  Copyright © 2016年 Orient. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
